7c9d7326d2687bd0b546209dfcb6a8034fda26e4
Diego <dflores7237@gmail.com>
2019-11-26 13:42:44 -0800

    made a clickable link button for all links

--- main.js
+++ main.js
@@ -7,11 +7,19 @@ $(document).ready(function() {
             var locationX = data[i]["location"][0];
             var locationy = data[i]["location"][1];
             var url = "https://www.google.com/maps?q=" + locationX + "," + locationy;
-            rowContent.push(name, description, url);
+            rowContent.push(name, description, "");
             var newTR = document.createElement("tr");
             for (x=0; x < rowContent.length; x++){
                 var newTD = document.createElement("td");
                 var tdContent = document.createTextNode(rowContent[x]);
+                if (x % 3 == 2){
+                    var newButton = document.createElement("button");
+                    var attr = document.createAttribute("onclick");
+                    attr.value = ("window.location='" + url + "'");
+                    newButton.setAttributeNode(attr);
+                    newButton.innerHTML = "click me";
+                    newTD.appendChild(newButton);
+                } 
                 newTD.appendChild(tdContent);
                 newTR.appendChild(newTD);
             }
