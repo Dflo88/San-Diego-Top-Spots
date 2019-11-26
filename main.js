92c4c653044840a59958f2a2705087108bc5806c
Diego <dflores7237@gmail.com>
2019-11-26 10:30:35 -0800

    Working solution with links displayed as text. There has been some styling done to the table

--- main.js
+++ main.js
@@ -1,4 +1,25 @@
 $(document).ready(function() {
-// write your code here
+    $.getJSON("data.json", function(data){
+        for (i=0; i < data.length; i++){
+            var rowContent = [];
+            var name = data[i]["name"];
+            var description = data[i]["description"];
+            var locationX = data[i]["location"][0];
+            var locationy = data[i]["location"][1];
+            var url = "https://www.google.com/maps?q=" + locationX + "," + locationy;
+            rowContent.push(name, description, url);
+            var newTR = document.createElement("tr");
+            for (x=0; x < rowContent.length; x++){
+                var newTD = document.createElement("td");
+                var tdContent = document.createTextNode(rowContent[x]);
+                newTD.appendChild(tdContent);
+                newTR.appendChild(newTD);
+            }
+            document.getElementById("main-table").appendChild(newTR);
+        }
+    });
+})
 
-});
+
+
+
